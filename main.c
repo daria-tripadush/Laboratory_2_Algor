7f2eea28b23e8ae108680ccb07da1b4a6cf57f98
Vadim Ostafiychuk <vadym.ostafiychuk@riseserv.com>
2020-05-14 21:14:20 +0300

    Release 1.0_Lab2

---
+++ main.c
@@ -0,0 +1,142 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+
+int main() 
+{
+
+	setlocale(LC_ALL, "ua");
+	int a;
+	int b;
+	
+	printf("Введiть номер завдання: (1-3)");
+	scanf("%d", &a);
+	if(a == 1) {
+		printf("Введiть тип завдання (1-2)");
+		scanf("%s", &b);
+		if(b == 1) {
+			exercOneA();
+		}
+		else if (b == 2) {
+			exercOne();
+		}
+		else {
+			printf("Такого типу завдяння 1 немає");
+		}
+	}
+	else if(a == 2) {
+		exercTwo();
+	}
+	else if(a == 3) {
+		exercThree();
+	}
+	else {
+		printf("Такого завдання немає");
+	}
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
+int exercOne ()
+{
+	int a;
+	float c,b;
+
+	printf("Введiть цiле число а= ");
+	scanf("%d", &a);
+	printf("Введiть дiйсне число с= ");
+	scanf("%f", &c);
+	
+
+	if((a) == 2)	
+	{
+		b = (pow(a,2)-4*c)/(a*c);
+	}
+	else if((a) == 4) 
+	{
+		b = sqrt(a*c-2*a);
+	}
+	else if (a == 6) 
+	{
+		b = pow(c,2)-2*a;
+	}
+	else {
+		printf("Неможливо обчислити значення виразу!");
+		return 0;
+	}
+
+	printf("Значення виразу %f", b);
+	return 0;
+}
+
+exercOneA() {
+	int a;
+	float c,b;
+
+	printf("Введiть цiле число а= ");
+	scanf("%d", &a);
+	printf("Введiть дiйсне число с= ");
+	scanf("%f", &c);
+
+	if(a == 2){ b = (pow(a,2)-4*c)/(a*c); printf("Значення виразу %f", b); return 0;} 
+	if(a == 4){ b = sqrt(a*c-2*a); printf("Значення виразу %f", b); return 0;} 
+	if (a == 6){ b = pow(c,2)-2*a;printf("Значення виразу %f", b); return 0;} 
+	
+	
+	printf("Неможливо обчислити значення виразу!");
+	return 0;
+}	
+
+int exercTwo() 
+{
+	int x,y;
+
+	printf("Введiть координати точки А\nx=");
+	scanf("%d",&x);
+	printf("y=");
+	scanf("%d",&y);
+
+	if((x*y>0)) {
+		if(x>0&&y>0) {
+			printf("1 kvadrant");
+		}
+		else {
+			printf("3 kvadrant");
+		}
+	}
+	else {
+		if(x>0&&y<0) {
+			printf("4 kvadrant");
+		}
+		else {
+			printf("2 kvadrant");
+		}
+	}
+	return 0;
+}
+
+int exercThree() {
+	int day;
+
+	printf("Введiть порядковий номер дня тижня: ");
+	scanf("%d", &day);
+
+
+	switch (day) {
+		case 1: printf("1-Понеділок");break; 
+		case 2: printf("2-Вівторок");break; 
+		case 3: printf("3-Середа");break; 
+		case 4: printf("4-Четвер");break; 
+		case 5: printf("5-П'ятниця");break; 
+		case 6: printf("6-Субота");break; 
+		case 7: printf("7-Недiля");break; 
+		default: printf("Такого дня немає");break;
+	}
+	printf("\n\n\n");
+	return 0;
+}
